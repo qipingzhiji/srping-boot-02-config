6095982f806a85ba04100ea085a8a7b57da1d81a
七凭之祭 <33921959+qipingzhiji@users.noreply.github.com>
2019-05-27 15:48:13 +0800

    Update Config.java

--- src/main/java/com/springboot/config/Config.java
+++ src/main/java/com/springboot/config/Config.java
@@ -8,10 +8,11 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration//标注这是一个配置类，可以认为是spring配置文件中的<beans><bean>属性
 public class Config {
-
+    
+    //由于是一个bean属性类，必须有返回，否则在使用的时候会报错
     @Bean(name = "helloService02")
-    public void helloService() {
-
+    public Config helloService() {
+        return new Config();
     }
 
 }
